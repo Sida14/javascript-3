b8a78d8c10276161733278d60384a103a996a671
Sida14 <164236903+Sida14@users.noreply.github.com>
2024-06-05 14:13:55 +0400

    Add files via upload

---
+++ javascript 3/script.js
@@ -0,0 +1,12 @@
+  let obieqti = {
+    firstname : "Levani" , 
+    lastname : "Sidamonidze",
+    age : "14",
+    adress : "tbilisi, didi digomi",
+    Gmail : [
+        {main : "sidamonidzelevani9@gmail.com",
+            second : "sidasida159@gmail.com",
+            third : "sidamonidzelevani211@gmail.com"
+         }
+    ]
+}
